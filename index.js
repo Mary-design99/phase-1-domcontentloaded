344fccd694c405870832a1c8aff9c0dd75e3c4bc
Mary-design99 <mary.nyarangi@student.moringaschool.com>
2025-06-17 22:52:31 +0300

    content

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Your code goes her
+document.addEventListener('DOMContentLoaded', () => {
+
+    const paragraphElement = document.querySelector('p');
+    if (paragraphElement) {
+        paragraphElement.textContent = "This is really cool!";
+    }
+});
